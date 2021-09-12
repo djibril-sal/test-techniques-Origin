cda5ef26f0a95e241a136a173237ddd8ccb0fcae
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:15:16 +0200

    Add files via upload

--- routes/Tag-Video.js
+++ routes/Tag-Video.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const TagVideo = require('../models/Tag-Video');
+const db = require('../config/database');
+const auth = require('../middleware/auth');
 
+const tagsVideoCtrl = require('../controllers/tag-videos');
+
+
+router.post('/', auth, tagsVideoCtrl.createVideo);
+router.delete('/:id', auth, tagsVideoCtrl.deleteVideo);
+
+module.exports = router;

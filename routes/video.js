60d79dbaa3b2b98ff03446d9784daf99658641e4
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:16:39 +0200

    Add files via upload

--- routes/video.js
+++ routes/video.js
@@ -1,1 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const Video = require('../models/Video');
+const db = require('../config/database');
+const auth = require('../middleware/auth');
 
+const videoCtrl = require('../controllers/video');
+
+router.get('/', auth, videoCtrl.getAllVideo);
+router.post('/', auth, videoCtrl.createVideo);
+router.get('/:id', auth, videoCtrl.getOneVideo);
+router.put('/:id', auth, videoCtrl.modifyVideo);
+router.delete('/:id', auth, videoCtrl.deleteVideo);
+
+module.exports = router;

fab21ce12f7628852d1f6f3302b7bab8ae722f42
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:03:14 +0200

    Add files via upload

--- controllers/tag-video.js
+++ controllers/tag-video.js
@@ -1,1 +1,40 @@
+const TagVideo = require('../models/Tags');
 
+exports.createVideo = (req, res, next) => {
+  const thing = new Thing({
+    id: req.body.id,
+    idVideo: req.body.idVideo,
+    idTags: req.body.idTags
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+
+
+exports.deleteVideo = (req, res, next) => {
+  Thing.deleteOne({id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};

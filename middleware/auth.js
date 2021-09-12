b752ca4824599dc04f6793552ad711e396495b9b
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:07:33 +0200

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,18 @@
+const jwt = require('jsonwebtoken');
 
+module.exports= (req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const decodesToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const userId = decodesToken.userId;
+        if(req.body.userId && req.body.userId != userId) {
+            throw 'User ID non valable';
+        } else {
+            next();
+        }
+
+
+    } catch (error) {
+        res.status(401).json({erro: error | 'Requete non authentifié'});
+    }
+};

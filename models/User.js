cf288bc9e0af20dac7bee7dd93f774a19d5645f2
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:10:58 +0200

    Add files via upload

--- models/User.js
+++ models/User.js
@@ -1,1 +1,12 @@
+const sequelize = require('sequelize');
+const db = require('../config/database');
+require('sequelize-isunique-validator')(sequelize);
 
+const user = db.define('User', {
+  email: { type: sequelize.STRING, required: true, isunique: true, allowNull: false, validate: {isEmail: true, isUnique: sequelize.validateIsUnique('email')}},
+  Password: { type: sequelize.STRING, required: true, isunique: true, allowNull: false }
+});
+
+userSchema.plugin(uniqueValidator);
+
+module.exports = sequelize.model('User', user);

dc4862b862fcd14e93fe15368beb63534760b686
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:11:37 +0200

    Add files via upload

--- models/Video.js
+++ models/Video.js
@@ -1,1 +1,19 @@
+const sequelize = require('sequelize');
+const db = require('../config/database');
 
+const Video = db.define('Video', {
+
+    idVideo:{type:sequelize.INTEGER, required:true, autoIncrement:true, allowNull:false, primaryKey:true},
+    idUsers:{type:sequelize.INTEGER, autoIncrement:true, allowNull:false, referencies:{model:'Users', key:'idVideo'}},
+    nom:{type:sequelize.STRING, required:true, allowNull:false},
+    description:{type:sequelize.STRING},
+    url:{type:sequelize.STRING, required:true, allowNull:false},
+    createdAt:{type:sequelize.STRING},
+    updateAt:{type:sequelize.STRING},
+
+});
+
+Video.sync().then(() => {
+    console.log('tableau crée !');
+  });
+  module.exports = sequelise.model('Video', Video);

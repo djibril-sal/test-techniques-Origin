a2542d411e999e45daf1ffc497dd5a6105044c5f
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:14:32 +0200

    Add files via upload

--- routes/Tags.js
+++ routes/Tags.js
@@ -1,1 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Sequelize = require('sequelize');
+const TagVideo = require('../models/Tags');
+const db = require('../config/database');
+const auth = require('../middleware/auth');
 
+const tagsCtrl = require('../controllers/tags');
+
+
+router.post('/', auth, tagsCtrl.createVideo);
+router.delete('/:id', auth, tagsCtrl.deleteVideo);
+
+module.exports = router;

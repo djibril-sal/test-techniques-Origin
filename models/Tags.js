211d94304a0e0c2fc13e50e64a5113b7bc57f85a
djibril-sal <78761473+djibril-sal@users.noreply.github.com>
2021-09-12 14:09:09 +0200

    Add files via upload

--- models/Tags.js
+++ models/Tags.js
@@ -1,1 +1,13 @@
+const sequelize = require('sequelize');
+const db = require('../config/database');
 
+const tags = db.define('Tag-Video',{
+
+    idTags:{type:sequelize.INTEGER, required:true, autoIncrement:true, allowNull:false, primaryKey:true},
+    valeur:{type:sequelize.NUMBER, required:true, allowNull:false,},
+    
+
+});
+
+
+module.exports = sequelize.model('Tags', tags);
